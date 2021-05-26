139e33e15ba407f125f79aef8edddee1bf7aaa06
atgiphy3750 <plastic041@gmail.com>
2021-05-26 19:14:46 +0900

    data: revert changes

--- app/__init__.py
+++ app/__init__.py
@@ -16,33 +16,39 @@ def index():
 
 @app.route("/data", methods=["POST"])
 def get_data():
+    # TODO: refactor this
+    return data()
+
     saved_json = os.path.join(app.static_folder, "weather.json")  # type:ignore
-    with open(saved_json, encoding="utf-8") as saved:
+    with open(saved_json, "w", encoding="utf-8") as saved:
         if os.path.exists(saved_json):
             try:
                 data_ = json.load(saved)
                 print("read")
             except io.UnsupportedOperation:
-                data_ = data()
-                json.dump(data_, saved)
-                print("empty file")
+                data_ = save_file(saved, "empty file")
                 return data_
             create_time_str = data_.get("0").get("createTime")
             if create_time_str:
-                create_time = datetime.strptime(create_time_str, r"%Y%m%d%H%M")
+                create_time = datetime.strptime("202005250500", r"%Y%m%d%H%M")
                 hour_diff = sub_hour(create_time, datetime.now())
                 if hour_diff < 3:
                     print("under 3 hours")
                     return data_
                 else:
-                    print("over 3 hours")
-                    return data()
+                    print("??")
+                    data_ = save_file(saved, "older than 3 hours")
+                    return data_
         else:
-            print("no file exists")
-            data_: dict = data()
-            json.dump(data_, saved)
+            data_ = save_file(saved, "cannot find file")
             return data_
-    # TODO: refactor this
+
+
+def save_file(saved: io.TextIOWrapper, say: str):
+    data_ = data()
+    json.dump(data_, saved)
+    print(say)
+    return data_
 
 
 def sub_hour(before: datetime, after: datetime):
